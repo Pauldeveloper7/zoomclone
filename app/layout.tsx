86fd8f618e825a57a22fc60724cc7a159c7a703e
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-04-19 16:40:24 +0530

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
-
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 export const metadata: Metadata = {
   title: "YOOM",
   description: "Video calling App",
